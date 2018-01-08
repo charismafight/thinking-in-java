272d8cae75a5927932b6778dd1472a0bcd161983
charismafight@hotmail.com <charismafight@hotmail.com>
2018-01-08 22:47:12 +0800

    init

---
+++ src/com/sangou/Cake.java
@@ -0,0 +1,8 @@
+package com.sangou;
+
+public class Cake {
+    public void callPie() {
+        Pie p = new Pie();
+        p.f();
+    }
+}
---
+++ src/com/sangou/Pie.java
@@ -0,0 +1,7 @@
+package com.sangou;
+
+public class Pie {
+    public void f() {
+        System.out.println("call f method");
+    }
+}
