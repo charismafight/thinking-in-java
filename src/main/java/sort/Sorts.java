f794f149ff4ed6c3bb5e04377be1325e47828b05
charismafight@hotmail.com <charismafight@hotmail.com>
2018-01-17 22:15:12 +0800

    quicksort

--- src/main/java/sort/Sorts.java
+++ src/main/java/sort/Sorts.java
@@ -1,7 +1,16 @@
 package sort;
 
 
+import javax.xml.transform.Result;
+
 public class Sorts {
+    public static void main(String[] args) {
+        int[] result = quickSort(new int[]{3, 2, 234, 64, 5, 768, 3, 5, 7, 4643, 6});
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+
     public static int[] insertSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             //从第二个数开始往后走，缓存下当前这个数来找地方插入
@@ -14,5 +23,36 @@ public class Sorts {
             arr[before + 1] = current;
         }
         return arr;
+    }
+
+    public static int[] quickSort(int[] arr) {
+        _quickSort(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private static void _quickSort(int[] arr, int from, int to) {
+        if (from >= to) {
+            return;
+        }
+
+        int target = arr[to];
+        int pos_for_target = from;
+        for (int i = from; i < to; i++) {
+            if (arr[i] < target) {
+                int tmp = arr[i];
+                arr[i] = arr[pos_for_target];
+                arr[pos_for_target] = tmp;
+                pos_for_target++;
+            }
+        }
+
+        int tmp = arr[pos_for_target];
+        arr[pos_for_target] = arr[to];
+        arr[to] = tmp;
+
+        _quickSort(arr, from, pos_for_target - 1);
+        _quickSort(arr, pos_for_target + 1, to);
+
+        return;
     }
 }
